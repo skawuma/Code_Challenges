b4647b93cce0d1a3436e38ecb48ce180433e1661
skawuma <82976186+skawuma@users.noreply.github.com>
2023-08-03 16:59:38 -0400

    started on Backend

---
+++ ChristianBook/src/main/java/com/ch_book/ChristianBook/ChristianBookApplication.java
@@ -0,0 +1,13 @@
+package com.ch_book.ChristianBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristianBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristianBookApplication.class, args);
+	}
+
+}
---
+++ ChristianBook/src/test/java/com/ch_book/ChristianBook/ChristianBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch_book.ChristianBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristianBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
