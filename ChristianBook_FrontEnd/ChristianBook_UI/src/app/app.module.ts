55d7a58ec71bbf84ce5573581823f0acdfb1f094
skawuma <82976186+skawuma@users.noreply.github.com>
2023-08-04 19:39:08 -0400

    deleted module

--- ChristianBook_FrontEnd/ChristianBook_UI/src/app/app.module.ts
+++ ChristianBook_FrontEnd/ChristianBook_UI/src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    HeaderComponent,
+    UserComponent,
+
+    AdminComponent,
+    
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+   MatToolbarModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
